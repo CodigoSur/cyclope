f950a809593bd5b29c01820f4393274644358838
Alicia Viana <m.aliciaviana@gmail.com>
2010-05-11 19:04:53 -0300

    agregamos tema somosregion

---
+++ cyclope/templates/cyclope/themes/somosregion/__init__.py
@@ -0,0 +1,39 @@
+# *-- coding:utf-8 --*
+"""Configuration for the Potente theme templates and regions.
+
+Attributes:
+    verbose_name: name of theme to be displayed in the admin interface
+    layout_templates: dictionary defining regions for the available templates
+"""
+
+from django.utils.translation import ugettext as _
+
+verbose_name = _('Somos Region')
+
+layout_templates = {
+
+    'one_sidebar.html':
+        {
+        'verbose_name': _('One sidebar'),
+        'regions' : {
+            'header': _('header'),
+            'left_sidebar': _('left sidebar'),
+            'before_content': _('before content'),
+            'after_content': _('after content'),
+            'footer': _('footer'),
+            }
+        },
+
+    'two_sidebars.html':
+        {
+        'verbose_name': _('Two sidebars'),
+        'regions' : {
+            'header': _('header'),
+            'left_sidebar': _('left sidebar'),
+            'right_sidebar': _('right sidebar'),
+            'before_content': _('before content'),
+            'after_content': _('after content'),
+            'footer': _('footer'),
+            }
+        },
+}
