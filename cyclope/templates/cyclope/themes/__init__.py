a35c0878e3bfe2fab640ed2da8b115be1e619ded
nicoechaniz <nicoechaniz@codigosur.org>
2010-05-14 16:40:25 -0300

    fixed local themes settings

--- cyclope/templates/cyclope/themes/__init__.py
+++ cyclope/templates/cyclope/themes/__init__.py
@@ -17,22 +17,26 @@ available = [ item for item in os.listdir(
 
 local_themes = []
 if hasattr(settings, 'CYCLOPE_LOCAL_THEMES_DIR'):
-    print "has"
     if not hasattr(settings, 'CYCLOPE_LOCAL_THEMES_MEDIA_PREFIX'):
         raise ImproperlyConfigured(
          _('You need to set CYCLOPE_LOCAL_THEMES_PREFIX in your settings file'))
     local_themes_dir = settings.CYCLOPE_LOCAL_THEMES_DIR
-    sys.path.append(local_themes_dir)
-    print local_themes_dir
-    local_themes = [
-        item for item in
-        os.listdir(os.path.join(os.path.dirname(local_themes_dir)))
-        if not item.startswith('__init__.py') and not item in available ]
 
-    available.extend(local_themes)
+    #TODO(nicoechaniz): we need to further check that this dir has valid theme info
+    if os.path.exists(local_themes_dir):
+        sys.path.append(local_themes_dir)
+        local_themes = [
+            item for item in
+            os.listdir(os.path.join(os.path.dirname(local_themes_dir)))
+            if not item.startswith('__init__.py') and not item in available ]
+
+        available.extend(local_themes)
+    else:
+        raise ImproperlyConfigured(
+         _('Your CYCLOPE_LOCAL_THEMES_DIR setting is invalid'))
+
 
 for theme in available:
 #    importlib.import_module(theme)
     print theme
     exec 'import %s' % theme
-
