0b00ae58fe9e4262a041e839dba6ac75b413b8ad
nicoechaniz <nicoechaniz@codigosur.org>
2010-09-09 22:57:12 -0300

    preliminary version of Polls app

---
+++ cyclope/apps/polls/frontend_views.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2010 Código Sur - Nuestra América Asoc. Civil / Fundación Pacificar.
+# All rights reserved.
+#
+# This file is part of Cyclope.
+#
+# Cyclope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cyclope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from django.utils.translation import ugettext_lazy as _
+from django.template import loader, RequestContext
+from django.http import Http404, HttpResponse
+from django.forms.formsets import formset_factory
+from django.shortcuts import redirect
+
+from cyclope.core import frontend
+from cyclope.utils import template_for_request
+from cyclope import views
+
+from models import Poll, Submission, Question
+from forms import QuestionForm
+
+class PollSubmissionView(frontend.FrontendView):
+    """Show or process a Poll Submission form"""
+
+    name = 'submission'
+    verbose_name=_('Poll submission form')
+
+    def get_http_response(self, request, slug=None, *args, **kwargs):
+        forms = []
+        
+        poll = Poll.objects.get(slug=slug)
+        questions = Question.objects.filter(poll=poll)
+        for question in questions:
+            form = QuestionForm(question, data=request.POST or None, prefix=question.pk)
+            forms.append({'question': question.text, 'form': form})
+
+        if all( (d['form'].is_valid() for d in forms) ):
+            posted_answers = []
+            for d in forms:
+                posted_answers.extend(d['form'].get_answers())
+            submission = Submission(poll=poll)
+            submission.save()
+            print posted_answers
+            submission.answers = posted_answers
+            submission.save()
+            return redirect('/poll/%s' % poll.slug)
+            
+
+        else:
+            t = loader.get_template("polls/poll_submission.html")
+            c = RequestContext(request,
+                               {'host_template': template_for_request(request),
+                                'poll': Poll.objects.get(slug=slug),
+                                'forms': forms,
+                                })
+            return HttpResponse(t.render(c))
+
+frontend.site.register_view(Poll, PollSubmissionView())
+
+
+class PollDetailView(frontend.FrontendView):
+    """Detail view for Polls"""
+    name = 'detail'
+    verbose_name=_('detailed view of the selected Poll')
+    is_default = True
+    params = {'queryset': Poll.objects,
+              'template_object_name': 'poll',
+             }
+
+    def get_http_response(self, request, slug=None, *args, **kwargs):
+        return views.object_detail(request, slug=slug,
+                                   inline=False, *args, **kwargs)
+    
+frontend.site.register_view(Poll, PollDetailView())
+
---
+++ cyclope/apps/polls/models.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2010 Código Sur - Nuestra América Asoc. Civil / Fundación Pacificar.
+# All rights reserved.
+#
+# This file is part of Cyclope.
+#
+# Cyclope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cyclope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+apps.polls.models
+--------------------
+"""
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+from cyclope.models import BaseContent
+from cyclope.core.collections.models import Collectible
+
+class Poll(BaseContent, Collectible):
+    description = models.TextField(_('description'), blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('poll')
+        verbose_name_plural = _('polls')
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll, editable=False)
+    text = models.CharField(_('text'), blank=False,
+                            db_index=True, max_length=255)
+    order = models.IntegerField(null=True, blank=True)
+    allow_multiple_answers = models.BooleanField(
+        _('allow multiple answers'), default=False)
+    #allow_unlisted = models.BooleanField(
+    #    _('allow unlisted answer'), default=True)
+
+    def __unicode__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = _('question')
+        verbose_name_plural = _('questions')
+        ordering = ('order',)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, verbose_name=_('question'))
+    text = models.CharField(_('text'), blank=False,
+                            db_index=True, max_length=255)
+    def get_votes(self):
+        return Submission.objects.filter(answers=self).count()
+    #unlisted = models.BooleanField(default=False, editable=False, db_index=True)
+
+    def __unicode__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = _('answer')
+        verbose_name_plural = _('answers')
+
+
+class Submission(models.Model):
+    poll = models.ForeignKey(Poll)
+    user = models.ForeignKey(User, verbose_name=_('user'), null=True)
+    foreign_user = models.CharField(_('foreign_user'), blank=True,
+                                    db_index=True, max_length=60)
+    answers = models.ManyToManyField(Answer, verbose_name=_('answers'))
+
+    class Meta:
+        verbose_name = _('submission')
+        verbose_name_plural = _('submissions')
+        ordering = ('poll', 'user')
