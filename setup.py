65cec0aa36872a405d3c79f50c6697f523901124
nicoechaniz <nicoechaniz@codigosur.org>
2010-10-16 09:17:17 -0300

    change mptt dependency untill we adapt to the change of version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'Django>=1.2',
         'FeinCms>=1.0.99,==dev',
         'django-autoslug>=1.4.1',
-        'django-mptt>=0.3-pre,==dev',
+        'django-mptt<0.4',
         'PIL>=1.1.7',
         'django-simple-captcha>=0.1.7',
         'django-tagging>=0.3.1',
