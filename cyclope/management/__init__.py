2f367287fc1399589481c2ba7d7748b804c4ebbe
Santiago Piccinini <spiccinini@codigosur.org>
2011-01-14 15:32:50 -0300

    Fix post_syncdb signal for Site creation.

--- cyclope/management/__init__.py
+++ cyclope/management/__init__.py
@@ -1,11 +1,11 @@
 from django.db.models import signals
-from south.signals import post_migrate
 import cyclope
 
 def create_site(app, created_models, verbosity, db, **kwargs):
     from django.contrib.sites.models import Site
     from cyclope.models import Menu, MenuItem, Layout, SiteSettings
-    if all([model in created_models for model in (Menu, MenuItem, Layout, SiteSettings)]):
+    if all([model in created_models for model in (Menu, MenuItem, Layout, SiteSettings)]) and \
+        not Menu.objects.all():
         msg = "\nDomain name (leave empty for default: localhost:8000): "
         domain = raw_input(msg)
         if not domain:
@@ -19,21 +19,25 @@ def create_site(app, created_models, verbosity, db, **kwargs):
         site.name = name
         site.save(using=db)
 
-        menu = Menu(name="Main menu", main_menu=True)
-        menu.save(using=db)
+        menu, created = Menu.objects.get_or_create(name="Main menu", main_menu=True)
+        if created:
+            menu.save(using=db)
 
-        layout = Layout(name="default", template='one_sidebar.html')
-        layout.save(using=db)
+        layout, created = Layout.objects.get_or_create(name="default", template='one_sidebar.html')
+        if created:
+            layout.save(using=db)
 
-        menu_item = MenuItem(menu=menu, name="home", site_home=True,
-                             active=True, layout=layout)
-        menu_item.save(using=db)
+        menu_item, created = MenuItem.objects.get_or_create(menu=menu, name="home",
+                                        site_home=True, active=True, layout=layout)
+        if created:
+            menu_item.save(using=db)
 
-        site_settings = SiteSettings(site=site,
-                                theme="neutronica",
-                                default_layout=layout,
-                                allow_comments='YES')
-        site_settings.save(using=db)
+        site_settings, created = SiteSettings.objects.get_or_create(site=site,
+                                                                theme="neutronica",
+                                                                default_layout=layout,
+                                                                allow_comments='YES')
+        if created:
+            site_settings.save(using=db)
 
 def create_user_groups(app, created_models, verbosity, db, **kwargs):
     from django.contrib.auth.models import User, Group
@@ -44,6 +48,6 @@ def create_user_groups(app, created_models, verbosity, db, **kwargs):
                 group.save(using=db)
 
 
-post_migrate.connect(create_site, sender=cyclope)
+signals.post_syncdb.connect(create_site)
 
 signals.post_syncdb.connect(create_user_groups, dispatch_uid="create_cyclope_user_groups")
