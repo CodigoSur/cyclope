dc2ca3e600982fd5e20a24a83fb6f36380d6d754
Santiago Piccinini <spiccinini@codigosur.org>
2011-05-01 11:47:33 -0300

    added handlers of error views in cyclope project template urls.py.

--- cyclope/conf/project_template/urls.py
+++ cyclope/conf/project_template/urls.py
@@ -8,6 +8,9 @@ frontend.autodiscover()
 from django.contrib import admin
 admin.autodiscover()
 
+handler500 = 'cyclope.views.error_500'
+handler404 = 'cyclope.views.error_404'
+
 urlpatterns = patterns('',
     (r'', include('cyclope.default_urls')),
     (r'^%s' % cyc_settings.CYCLOPE_PREFIX,
