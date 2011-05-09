ad37554ec9b3e5742e3e2fc5cea6a90be5cf63f3
Santiago Piccinini <spiccinini@codigosur.org>
2011-05-09 11:38:13 -0300

    added --directory option to startcyclopeproject

--- cyclope/management/commands/startcyclopeproject.py
+++ cyclope/management/commands/startcyclopeproject.py
@@ -5,9 +5,9 @@ import os
 import re
 import sys
 from random import choice
+from optparse import make_option
 
 import django
-
 from django.core import management
 from django.core.management.base import copy_helper, CommandError, LabelCommand
 from django.utils.importlib import import_module
@@ -15,9 +15,18 @@ from django.utils.importlib import import_module
 import cyclope
 
 class Command(LabelCommand):
-    help = "Creates a Cyclope project directory structure for the given project name in the current directory."
+    help = "Creates a Cyclope project directory structure for the given " \
+           "project name in the current directory."
     args = "[projectname]"
     label = 'project name'
+
+    option_list = LabelCommand.option_list + (
+        make_option('--directory', action='store', dest='directory',
+            default=".", help='Destination directory '
+                'where the project will be created.'),
+    )
+
+
 
     requires_model_validation = False
     # Can't import settings during this command, because they haven't
@@ -25,9 +34,7 @@ class Command(LabelCommand):
     can_import_settings = False
 
     def handle_label(self, project_name, **options):
-        # Determine the project_name a bit naively -- by looking at the name of
-        # the parent directory.
-        directory = os.getcwd()
+        directory = options.get("directory")
 
         # Check that the project_name cannot be imported.
         try:
@@ -35,7 +42,10 @@ class Command(LabelCommand):
         except ImportError:
             pass
         else:
-            raise CommandError("%r conflicts with the name of an existing Python module and cannot be used as a project name. Please try another name." % project_name)
+            msg = "%r conflicts with the name of an existing Python module "\
+                  "and cannot be used as a project name. " \
+                  "Please try another name."
+            raise CommandError(msg % project_name)
         # monkeypatch django.__path__ because the internal use in copy_helper
         django_path = django.__path__[:]
         django.__path__[0] = cyclope.__path__[0]
@@ -75,8 +85,11 @@ class Command(LabelCommand):
         settings_contents = re.sub(r"(?<=SECRET_KEY = ')'", secret_key + "'", settings_contents)
         fp.write(settings_contents)
         fp.close()
-
-        print "Now you should run ./manage.py syncdb and then ./manage.py migrate"
+        print u"\nProject created successfully\n"
+        sync_msg = u"Now you must cd to %s and run './manage.py syncdb' " \
+                   u"and then './manage.py migrate' to create an empty "  \
+                   u"database" % top_dir
+        print sync_msg
 
 if __name__ == "__main__":
     cmnd = Command()
