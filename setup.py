5b8a4f95bee0ae55c05fd4aa0b5ebe4df1abfa2a
Santiago Piccinini <spiccinini@codigosur.org>
2012-04-04 10:56:00 -0300

    Fixed django requirements to Django==1.3.1

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     ],
 
     install_requires=[
-        'Django>=1.3',
+        'Django==1.3.1',
         'FeinCms==1.3.0',
         'django-autoslug==1.4.1',
         'django-mptt==0.4.2', # 0.4 breaks compatibility
