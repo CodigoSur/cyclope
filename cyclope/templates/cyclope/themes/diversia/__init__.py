e55e2650c865cf4bbddc934487f371b6818ca2a4
San Hoerth <san@codigosur.org>
2012-07-30 20:58:39 -0500

    Added Diversia Theme

---
+++ cyclope/templates/cyclope/themes/diversia/__init__.py
@@ -0,0 +1,52 @@
+# *-- coding:utf-8 --*
+"""Configuration for the Neutrona theme templates and regions.
+
+Attributes:
+    verbose_name: name of theme to be displayed in the admin interface
+    layout_templates: dictionary defining regions for the available templates
+"""
+
+from django.utils.translation import ugettext_lazy as _
+
+verbose_name = _('Diversia')
+
+layout_templates = {
+
+    'four_elements.html':
+        {
+        'verbose_name': _('FourElements'),
+        'regions' : {
+            'air': _('air'),
+            'water': _('water'),
+            'spark': _('Spark'),
+            'ash': _('Ash'),
+            'earth': _('earth'),
+            }
+        },
+
+    'five_elements.html':
+        {
+        'verbose_name': _('FiveElements'),
+        'regions' : {
+            'air': _('Air'),
+            'water': _('Water'),
+            'love': _('Love'),
+            'spark': _('Spark'),
+            'ash': _('Ash'),
+            'earth': _('Earth'),
+            }
+        },
+    'empty.html':
+        {'verbose_name': _('Empty'),
+         'regions': {}
+         },
+
+    'newsletter.html':
+        {'verbose_name': _('Newsletter'),
+        'regions' : {
+            'header': _('header'),
+            'before_content': _('before content'),
+            'after_content': _('after content'),
+            }
+         },
+}
