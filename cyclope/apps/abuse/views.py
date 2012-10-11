f2a301c759dcde2faef313cff1b5d8016456a00b
Santiago Piccinini <spiccinini@codigosur.org>
2012-10-11 20:34:30 -0300

    Added crispy to polls, abuse and search.

--- cyclope/apps/abuse/views.py
+++ cyclope/apps/abuse/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.decorators import login_required
-
+from cyclope.utils import CrispyFormsSimpleMixin
 from models import AbuseReportElement
 
 
-class ReportElementForm(forms.ModelForm):
+class ReportElementForm(forms.ModelForm, CrispyFormsSimpleMixin):
     class Meta:
         model = AbuseReportElement
         fields = ('abuse_type', 'text')
@@ -15,6 +15,7 @@ class ReportElementForm(forms.ModelForm):
 
 @login_required
 def abuse_report(request, ct_id, obj_id):
+    #TODO: validate unique of user, ct_id, obj_id
     if request.method == "POST":
         form = ReportElementForm(request.POST)
         if form.is_valid():
@@ -23,7 +24,7 @@ def abuse_report(request, ct_id, obj_id):
             report_el.content_object = klass.objects.get(pk=obj_id)
             report_el.user = request.user
             report_el.save()
-            return render(request, "abuse/report.html")
+            return render(request, "abuse/report.html", {"content": report_el.content_object})
     else:
         form = ReportElementForm()
 
