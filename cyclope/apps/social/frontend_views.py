344c6574355265e329c8ede430e236662d63d462
Santiago Piccinini <spiccinini@codigosur.org>
2013-07-03 13:58:47 -0300

    Added Social's Global Only Activity view

--- cyclope/apps/social/frontend_views.py
+++ cyclope/apps/social/frontend_views.py
@@ -3,16 +3,11 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.template.loader import render_to_string
-from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
-from django.template.loader import select_template, get_template
-from django.template import TemplateDoesNotExist
 from django.core.paginator import Paginator
 from actstream.models import Action, target_stream, user_stream
 
 from cyclope.core import frontend
 import cyclope.utils
-from cyclope.templatetags.cyclope_utils import inline_template
 
 class Social(models.Model):
     """Fake model for cyclope frontend view registration"""
@@ -20,7 +15,7 @@ class Social(models.Model):
 
 class GlobalActivity(frontend.FrontendView):
     name = 'global_activity'
-    verbose_name = _('Global activity of the site')
+    verbose_name = _("Global activity or user's feed if authenticated")
     is_default = True
     is_instance_view = False
     is_region_view = False
@@ -28,15 +23,32 @@ class GlobalActivity(frontend.FrontendView):
     template = "social/actions_list.html"
 
     def get_response(self, request, req_context, options):
-        if request.user.is_authenticated():
-            actions = target_stream(request.user) | user_stream(request.user)
-        else:
-            actions = Action.objects.public()
-
-        paginator = Paginator(actions, per_page=10)
-        page = cyclope.utils.get_page(paginator, request)
+        actions = self.get_actions(request)
+        page = self.build_page(request, actions)
         return render_to_string(self.template, {
             'page': page,
         }, req_context)
 
+    def get_actions(self, request):
+        if request.user.is_authenticated():
+            actions = target_stream(request.user) | user_stream(request.user)
+        else:
+            actions = Action.objects.public()
+        return actions
+
+    def build_page(self, request, actions):
+        paginator = Paginator(actions, per_page=10)
+        page = cyclope.utils.get_page(paginator, request)
+        return page
+
+
+class GlobalOnlyActivity(GlobalActivity):
+    name = 'global_only_activity'
+    verbose_name = _('Global activity of the site')
+    is_default = False
+
+    def get_actions(self, request):
+        return Action.objects.public()
+
 frontend.site.register_view(Social, GlobalActivity)
+frontend.site.register_view(Social, GlobalOnlyActivity)
