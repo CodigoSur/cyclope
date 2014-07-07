c44839404487da3dd559393334c3cfe6f8b15337
Santiago Piccinini <spiccinini@codigosur.org>
2014-07-07 11:45:43 -0300

    Added button to remove the image of an article

--- cyclope/apps/articles/admin.py
+++ cyclope/apps/articles/admin.py
@@ -64,6 +64,7 @@ from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 
@@ -74,10 +75,11 @@ from cyclope.apps.related_admin import GenericModelChoiceField as GMCField
 class SearchAndCreateFKWidget(GenericFKWidget):
     def render(self, name, value, attrs):
         out = super(SearchAndCreateFKWidget, self).render(name, value, attrs)
+        remove_picture = """<div class='remove_picture' onClick="$('#id_%s').val('');$('#id_%s').siblings('.object-representation').empty();">%s</div>""" % (name, name, ugettext(u"Remove"))
         name = "%s_none" % name
         picture_content_type_id = ContentType.objects.get_by_natural_key("medialibrary", "picture").id
         fake_ctfield = "<select id='id_{0}' name='{0}' style='display:none'><option value='{1}'>Picture</option></select>".format(name, picture_content_type_id)
-        return mark_safe("<fieldset class='inlined'>" + fake_ctfield + out + "</fieldset>")
+        return mark_safe("<fieldset class='inlined'>" + fake_ctfield + out + remove_picture + "</fieldset>")
 
     def get_actual_object(self, value):
         try:
