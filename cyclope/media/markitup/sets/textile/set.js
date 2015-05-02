f559dc535ab8c389e6d6a08559ec188da3bb70ad
Santiago GG <santiago@codigosur.org>
2015-05-02 17:54:33 -0600

    added features markitup textile

--- cyclope/media/markitup/sets/textile/set.js
+++ cyclope/media/markitup/sets/textile/set.js
@@ -25,15 +25,25 @@ mySettings = {
 		{name:'Bold', key:'B', closeWith:'*', openWith:'*'},
 		{name:'Italic', key:'I', closeWith:'_', openWith:'_'},
 		{name:'Stroke through', key:'S', closeWith:'-', openWith:'-'},
+		{name:'Em dash', key:'D', closeWith:'--', openWith:'--'},
+		{name:'Underlined', key:'U', closeWith:'+', openWith:'+'},
+		{separator:'---------------' },
+		{name:'Align left', openWith:'p<. '},
+		{name:'Align right', openWith:'p>. '},
+		{name:'Align centered', openWith:'p=. '},
+		{name:'Align justified', openWith:'p<>. '},
 		{separator:'---------------' },
 		{name:'Bulleted list', openWith:'(!(* |!|*)!)'},
 		{name:'Numeric list', openWith:'(!(# |!|#)!)'},
+		{name:'Text indent', openWith:'p((. '},
 		{separator:'---------------' },
 		{name:'Picture', replaceWith: function(markItUp) {}},
 		{name:'Link', openWith:'"', closeWith:'([![Title]!])":[![Link:!:http://]!]', placeHolder:'Your text to link here...' },
 		{separator:'---------------' },
 		{name:'Quotes', openWith:'bq(!(([![Class]!])!)). '},
 		{name:'Code', openWith:'@', closeWith:'@'},
+/**		Hack for especials links with % **/
+		{name:'Especial link HTML', openWith:'<notextile><a title="[![Title]!]" href="[![Link:!:http://]!]" target="_blank">', closeWith:'</a></notextile>', placeHolder:'Your especial link here...' },
 //		{separator:'---------------' },
 //		{name:'Preview', call:'preview', className:'preview'}
 	]
@@ -51,7 +61,7 @@ to the edittor.
 var FileBrowserHelper = {
     markItUp: false, // objet to store id of the textarea clicked
     insertPicture: function(markItUp) {
-                $("#markItUp"+markItUp.capitalize() +" .markItUpButton13").click(function (){
+                $("#markItUp"+markItUp.capitalize() +" .markItUpButton20").click(function (){
                     FileBrowserHelper.markItUp = markItUp;
                     var id2 = String(markItUp).replace(/\-/g,"____").split(".").join("___");
                     FBWindow = window.open('/admin/filebrowser/browse/?pop=1&type=',
