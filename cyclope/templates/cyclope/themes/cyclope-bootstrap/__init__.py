ea0fd4ca27992b521680a872dc161fa0a05f727e
San Hoerth <san@codigosur.org>
2015-06-06 21:52:19 -0500

    added Responsive Theme Bootstrap based

---
+++ cyclope/templates/cyclope/themes/cyclope-bootstrap/__init__.py
@@ -0,0 +1,53 @@
+# *-- coding:utf-8 --*
+"""Configuration for the Neutrona theme templates and regions.
+
+Attributes:
+    verbose_name: name of theme to be displayed in the admin interface
+    layout_templates: dictionary defining regions for the available templates
+"""
+
+from django.utils.translation import ugettext_lazy as _
+
+verbose_name = _('Cyclope-Bootstrap')
+
+layout_templates = {
+
+    'default.html':
+        {
+        'verbose_name': _('Default'),
+        'regions' : {
+            'header': _('Header'),
+            'left': _('Left'),
+            'right': _('right'),
+            'up': _('Up'),
+            'bottom': _('Bottom'),
+            'footer': _('Footer'),
+            }
+        },
+
+    'main.html':
+        {
+        'verbose_name': _('Main'),
+        'regions' : {
+            'header': _('Header'),
+            'left': _('Left'),
+            'right': _('right'),
+            'up': _('Up'),
+            'bottom': _('Bottom'),
+            'footer': _('Footer'),
+            }
+        },
+    'empty.html':
+        {'verbose_name': _('Empty'),
+         'regions': {}
+         },
+
+    'newsletter.html':
+        {'verbose_name': _('Newsletter'),
+        'regions' : {
+            'header': _('header'),
+            'before_content': _('before content'),
+            'after_content': _('after content'),
+            }
+         },
+}
