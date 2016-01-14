578c0c94dd78b5675757c3a7f866bedb3a662c65
Roberto <roberto@codigosur.org>
2016-01-14 18:34:15 -0300

    actually uploading the image and associating it to filebrowsefield article relation and some logic still missing

--- cyclope/apps/media_widget/views.py
+++ cyclope/apps/media_widget/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 from cyclope.apps.medialibrary.models import Picture
 from cyclope.apps.medialibrary.forms import InlinedPictureForm
 from django.views.decorators.http import require_POST
 from django.core.urlresolvers import reverse
 from forms import MediaWidgetForm
+from filebrowser.functions import handle_file_upload, convert_filename
+from django.conf import settings
+import os
 
 # GET /pictures/new
 def pictures_upload(request):
@@ -14,14 +17,28 @@ def pictures_upload(request):
     return render(request, 'media_widget/pictures_upload.html', {'form': form})
 
 # POST /pictures/create
+#TODO use chunks with FileBrowseField?
 @require_POST
 def pictures_create(request):
     if request.user.is_staff:
         form = MediaWidgetForm(request.POST, request.FILES)
         if form.is_valid():
-            obj = form.save()
-            #TODO associate image with Picture's FileBrowseField
-            return HttpResponseRedirect(reverse('pictures-create')) #POST/Redirect/GET
+            image = form.cleaned_data['image']
+            #normalize file name
+            image.name = convert_filename(image.name)
+            #filesystem save
+            path = os.path.join(settings.MEDIA_ROOT, Picture._meta.get_field_by_name("image")[0].directory)
+            uploaded_path = handle_file_upload(path, image)
+            #database save
+            picture = Picture(
+                name = form.cleaned_data['name'],#TODO or image.name
+                description = form.cleaned_data['description'],#TODO or None
+                image = uploaded_path
+                #TODO user, etc.
+            )
+            picture.save()
+            #TODO associate picture with current Article
+            return redirect('pictures-create') #POST/Redirect/GET #TODO or just render with thumbnail?
         else:
             return render(request, 'media_widget/pictures_upload.html', {'form': form})
     else:
