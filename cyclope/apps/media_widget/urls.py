6856560984d5c855509515174a0d6dc74dbc5d8a
Roberto <roberto@codigosur.org>
2016-01-19 01:25:21 -0300

    select picture from media library working needs a real form so we can select multiple pictures in the future article admin's iframe made percent so we can resize the widget to see thumbails

--- cyclope/apps/media_widget/urls.py
+++ cyclope/apps/media_widget/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from views import pictures_upload, pictures_create
+from views import pictures_upload, pictures_create, pictures_update
 
 urlpatterns = patterns('',
     url(r'^pictures/new/(?P<article_id>\d+)$', pictures_upload, name="pictures-new"),
-    url(r'^pictures/create/(?P<article_id>\d+)$', pictures_create, name="pictures-create")     
+    url(r'^pictures/create/(?P<article_id>\d+)$', pictures_create, name="pictures-create"),
+    url(r'^pictures/update/(?P<article_id>\d+)$', pictures_update, name="pictures-update")
 )
--- cyclope/apps/media_widget/views.py
+++ cyclope/apps/media_widget/views.py
@@ -15,14 +15,22 @@ from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from cyclope.apps.articles.models import Article
 from cyclope.models import RelatedContent
 
-# GET /pictures/new
+# GET /pictures/new/article_id
 def pictures_upload(request, article_id):
     """ Returns widget's inner HTML to be viewed through an iframe.
         This ensures bootstrap styles isolation."""
+    #picture upload
     form = MediaWidgetForm()
-    return render(request, 'media_widget/pictures_upload.html', {'form': form, 'article_id': article_id})
+    #picture selection
+    pictures_list = Picture.objects.all().order_by('-creation_date')
+    #TODO MediaWidgetSelectForm to select multiple pictures
+    return render(request, 'media_widget/pictures_upload.html', {
+        'form': form, 
+        'article_id': article_id,
+        'pictures_list': pictures_list
+    })
 
-# POST /pictures/create
+# POST /pictures/create/article_id
 @require_POST
 def pictures_create(request, article_id):
     if request.user.is_staff:
@@ -47,19 +55,43 @@ def pictures_create(request, article_id):
                 source = article.source
             )
             picture.save()
-            #associate picture with current Article
-            article.picture = picture
-            article.save()
-            #article as Picture's related content
-            related = RelatedContent(
-                self_object = picture,
-                other_object = article
-            )
-            related.save()
+            _associate_picture_to_article(article, picture)
             messages.success(request, 'Imagen cargada: '+image.name)
             #POST/Redirect/GET
             return redirect('pictures-new', article_id)
         else:
-            return render(request, 'media_widget/pictures_upload.html', {'form': form, 'article_id': article_id})
+            #picture selection
+            pictures_list = Picture.objects.all().order_by('-creation_date')
+            return render(request, 'media_widget/pictures_upload.html', {
+                'form': form, 
+                'article_id': article_id,
+                'pictures_list': pictures_list
+            })
     else:
         return HttpResponseForbidden()
+        
+#POST /pictures/update/article_id
+@require_POST
+def pictures_update(request, article_id):
+    if request.user.is_staff:
+        article = Article.objects.get(pk=article_id)
+        picture_id = int(request.POST.get('picture_id'))
+        picture = Picture.objects.get(pk=picture_id)
+        _associate_picture_to_article(article, picture)
+        messages.success(request, 'Imagen seleccionada: '+picture.name)
+        return redirect('pictures-new', article_id) # POST/Redirect/GET 
+    else:
+        return HttpResponseForbidden()
+        
+#HELPERS
+def _associate_picture_to_article(article, picture):
+    """Helper method to DRY picture create and update"""
+    #associate picture with current Article
+    article.picture = picture
+    article.save()
+    #article as Picture's related content
+    related = RelatedContent(
+        self_object = picture,
+        other_object = article
+    )
+    related.save()
