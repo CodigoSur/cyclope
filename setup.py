dc6209fdffa375fdc8401331ee4f3a78e1277051
Roberto <roberto@codigosur.org>
2016-02-11 15:35:51 -0300

    django-simple-captcha requirement typo

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'django-mptt==0.4.2', # 0.4 breaks compatibility
         'django-mptt-tree-editor>=0.1.1,<0.2',
         'Pillow>=2.0',# python >= 2.6
-        'django-smple-captcha==0.4.1',
+        'django-simple-captcha==0.4.1',
         'django-filebrowser-nograpup>=3.0.3,<3.1',
         'South>=0.7,<0.8',
         'django-registration==0.8',
