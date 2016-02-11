8a457341d0d33ee58306f393e056c788ac366bfb
Roberto <roberto@codigosur.org>
2016-02-11 13:12:06 -0300

    deprecate PIL in favour of Pillow PIL is no longer in PyPi index and recently external packages were disallowed from it, PIL is unmaintained since 2013 and its fork Pillow has replaced it. Cyclope can safely transition thanks to the patch in default_settings.py and because it doesn't use the _imaging module.

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ setup(
         'django-autoslug==1.4.1',
         'django-mptt==0.4.2', # 0.4 breaks compatibility
         'django-mptt-tree-editor>=0.1.1,<0.2',
-        'PIL>=1.1.7', # in PIL we trust
-        'django-simple-captcha==0.4.1',
+        'Pillow>=2.0',# python >= 2.6
+        'django-smple-captcha==0.4.1',
         'django-filebrowser-nograpup>=3.0.3,<3.1',
         'South>=0.7,<0.8',
         'django-registration==0.8',
