99027708657347664105c9008e620870bbed1091
Roberto <roberto@codigosur.org>
2016-07-19 23:54:20 -0600

    fixes nRows in pagination footer

--- cyclope/apps/media_widget/static/media_widget/pagination_footer.js
+++ cyclope/apps/media_widget/static/media_widget/pagination_footer.js
@@ -1,15 +1,5 @@
-var query_str = '?n=';
 
-function set_query_str(n){
-    query_str += n;
-    query_str += '&nRows=' + $(this).val();;
-}
-
-function append_query_str_smooth(url){
-    return url+query_str;
-}
-
-function change_embed_widget(){
+function change_embed_widget(query_str){
     url = '/media_widget/library/'+$("#id_media_type").val()+query_str;
     $.get(url, function(data){
         $("#select_media_widget").html(data);
