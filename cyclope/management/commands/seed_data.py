f856121ccf011d37205895c234b14e5b83a1cb3a
Numerico <webmaster@numerica.cl>
2016-09-07 12:33:17 -0300

    contact form created by seed data only needs custom_url fixes #27

--- cyclope/management/commands/seed_data.py
+++ cyclope/management/commands/seed_data.py
@@ -128,11 +128,7 @@ class Command(BaseCommand):
         contact_menu_item = MenuItem(
             menu=menu, 
             name="Contacto", 
-            layout=default_layout, 
             custom_url="/contact",
-            content_object = contact,
-            content_view = "contents",
-            view_options = self.DEFAULT_VIEW_OPTIONS
         )
         contact_menu_item.save()
         # USER GROUPS
