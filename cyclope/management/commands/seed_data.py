1ff2ad864d7d459176a22b6f48cb197f34a8917a
Numerico <webmaster@numerica.cl>
2016-10-05 18:25:23 -0300

    dont translate layout names causes mistakes in seed_data

--- cyclope/management/commands/seed_data.py
+++ cyclope/management/commands/seed_data.py
@@ -16,27 +16,27 @@ class Command(BaseCommand):
 
     LYS = {
         'TRES COLUMNAS': {
-            'name': _('Three columns'),
+            'name': 'Three columns',
             'template': 'layout_three_columns.html',
             'image': 'layout_three_columns_thumbnail.png'
         },
         'DOS COLUMAS IZQUIERDA': {
-            'name': _('Two columns Left'),
+            'name': 'Two columns Left',
             'template': 'layout_two_columns_left.html',
             'image': 'layout_two_columns_left_thumbnail.png'
         }, 
         'DOS COLUMNAS DERECHA': {
-            'name': _('Two columns Right'),
+            'name': 'Two columns Right',
             'template': 'layout_two_columns_right.html',
             'image': 'layout_two_columns_right_thumbnail.png'
         },
         'MAGAZINE': {
-            'name': _('One column'),
+            'name': 'One column',
             'template': 'layout_one_column.html',
             'image': 'layout_one_column_thumbnail.png'
         },
         'NEWSLETTER': {
-            'name': _('Newsletter'),
+            'name': 'Newsletter',
             'template': 'newsletter.html',
             'image': 'layout_newsletters.png'
         }
