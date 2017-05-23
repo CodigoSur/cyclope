e32e5b33f727639084c06978c19f9be7aab27f32
Sebastian Oliva <code@sebastianoliva.com>
2017-05-23 12:44:53 -0600

    setting dropped categorization order based on previous and next sibling

--- cyclope/static/js/drag_drop.js
+++ cyclope/static/js/drag_drop.js
@@ -40,10 +40,34 @@ $(function(){
     $("body.categorizations-changelist table#result_list").disableSelection();
 });
 
+
 function updateChangelistOrder(event, ui) {
-    $('input[id$=order]').each(function (i, elem){
-        $(elem).val(i+1);
-    });
+    
+    //// The element that was dropped.
+    var droppedElement = event.toElement;
+    var prevElem = droppedElement.previousElementSibling;
+    var nextElem = droppedElement.nextElementSibling;
+    
+    //// Defaults for edge cases.
+    var prevVal = 0
+    var nextVal = 1
+    
+    if (prevElem != null) {
+        prevVal = parseFloat(prevElem.lastChild.firstChild.value);
+    }
+    if (nextElem != null) {
+        nextVal = parseFloat(nextElem.lastChild.firstChild.value);
+    }
+    
+    //console.log(prevVal)
+    //console.log(nextVal)
+    
+    //// Fixed on the previous edge, we set the new value to the mid point.
+    var newVal = prevVal + Math.abs((prevVal - nextVal)/2);
+    
+    $(droppedElement.lastChild.firstChild).val(newVal);
+    
+    //console.log(newVal);
 };
 
 // DYNAMIC FORMS
